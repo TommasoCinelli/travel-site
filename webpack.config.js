b523d06393f395233cb574c0cdf4bed117ea926e
Tommaso <tommasocinelli@gmail.com>
2020-08-21 16:02:22 +0200

    BEM chapter

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,35 @@
 const path = require('path')
 
 const postCSSPlugins = [
-    require('postcss-simple-vars'),
-    require('postcss-nested'),
-    require('autoprefixer')
+  require('postcss-import'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer')
 ]
 
 module.exports = {
-	entry: './app/assets/scripts/App.js',
-	output: {
-		filename: 'bundled.js',
-		path: path.resolve(__dirname, 'app')
-	},
-	mode: 'development',
-	watch: true,
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
-			}
-		]
-	}
+  entry: './app/assets/scripts/App.js',
+  output: {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
+  },
+  devServer: {
+    before: function(app, server) {
+      server._watch('./app/**/*.html')
+    },
+    contentBase: path.join(__dirname, 'app'),
+    hot: true, // no need to refresh
+    port: 3000,  // 80 80 as default
+    host: '0.0.0.0'
+  },
+  mode: 'development',
+  // watch: true, // no need because devServer
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+      }
+    ]
+  }
 }
